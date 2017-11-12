9a345cac6894677b1d262515a59f21cfc0d49920
alexandraalday <alexandra.alday@gmail.com>
2017-11-11 20:12:50 -0800

    draft panel

--- src/App.js
+++ src/App.js
@@ -1,18 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="panel panel-default app-content center-block">
+        <div className="panel-body">
+          <div className="container-fluid">
+            <div className="row">
+              <h2 className="text-center">Pokédoro</h2>
+            </div>
+            <div className="row">
+              <h2 className="text-center">00:00:00</h2>
+            </div>
+            <div className="row">
+              <h2 className="text-primary">Set Timer</h2>
+              <div className="row control-row">
+                <div className="form-group">
+                  <div className="col-sm-3">
+                    <label htmlFor="hours">Hours</label>
+                  </div>
+                  <div className="col-sm-9">
+                    <input id="hours" className="form-control" type="number" />
+                  </div>
+                </div>
+              </div>
+              <div className="row control-row">
+                <div className="form-group">
+                  <div className="col-sm-3">
+                    <label htmlFor="minutes">Minutes</label>
+                  </div>
+                  <div className="col-sm-9">
+                    <input id="minutes" className="form-control" type="number" />
+                  </div>
+                </div>
+              </div>
+              <div className="row control-row">
+                <div className="form-group">
+                  <div className="col-sm-3">
+                    <label htmlFor="seconds">Seconds</label>
+                  </div>
+                  <div className="col-sm-9">
+                    <input id="seconds" className="form-control" type="number" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
