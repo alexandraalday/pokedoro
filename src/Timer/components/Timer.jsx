d095bdbc1226e51864c248dcd46dc595bf6eeacf
alexandraalday <alexandra.alday@gmail.com>
2017-11-11 20:49:16 -0800

    refactor timer config component

--- src/Timer/components/Timer.jsx
+++ src/Timer/components/Timer.jsx
@@ -2,47 +2,14 @@ import React from 'react';
 import TimerHeader from '../../TimerHeader/components/TimerHeader';
 import TimerDisplay from '../../TimerDisplay/components/TimerDisplay';
 import TimerButton from '../../TimerButton/components/TimerButton';
-
+import TimerConfig from '../../TimerConfig/components/TimerConfig';
 
 const Timer = () => (
 	<div className="container-fluid">
 		<TimerHeader />
 		<TimerDisplay />
 		<TimerButton />
-
-		<div className="row">
-	  		<h2 className="text-primary">Set Timer</h2>
-	  		<div className="row control-row">
-	    		<div className="form-group">
-	      			<div className="col-sm-3">
-	        			<label htmlFor="hours">Hours</label>
-	     			</div>
-		      		<div className="col-sm-9">
-		        		<input id="hours" className="form-control" type="number" />
-		      		</div>
-		   		</div>
-		 	</div>
-		  	<div className="row control-row">
-		    	<div className="form-group">
-		     		<div className="col-sm-3">
-		        		<label htmlFor="minutes">Minutes</label>
-		      		</div>
-		      		<div className="col-sm-9">
-		        		<input id="minutes" className="form-control" type="number" />
-		      		</div>
-		    	</div>
-		 	</div>
-		  	<div className="row control-row">
-		    	<div className="form-group">
-		      		<div className="col-sm-3">
-		        		<label htmlFor="seconds">Seconds</label>
-		      		</div>
-		      		<div className="col-sm-9">
-		        		<input id="seconds" className="form-control" type="number" />
-		      		</div>
-		    	</div>
-		  	</div>
-		</div>
+		<TimerConfig />
 	</div>
 );
 
