90eb9cd959423f5c7cac3d5ce0cdb7daaa402cea
alexandraalday <alexandra.alday@gmail.com>
2017-11-16 10:31:44 -0800

    set timer states

--- src/Timer/components/Timer.jsx
+++ src/Timer/components/Timer.jsx
@@ -4,6 +4,7 @@ import TimerHeader from '../../TimerHeader/components/TimerHeader';
 import TimerDisplay from '../../TimerDisplay/components/TimerDisplay';
 import TimerButton from '../../TimerButton/components/TimerButton';
 import TimerConfig from '../../TimerConfig/components/TimerConfig';
+import * as timerStates from '../../timerStates';
 
 
 class Timer extends Component {
@@ -13,6 +14,7 @@ class Timer extends Component {
 		this.state = {
 			currentTime: moment.duration(25, 'minutes'),
 			baseTime: moment.duration(25, 'minutes'),
+			timerState: timerStates.NOT_SET,
 		};
 
 		this.setBaseTime = this.setBaseTime.bind(this);
@@ -31,10 +33,14 @@ class Timer extends Component {
 				<TimerHeader />
 				<TimerDisplay currentTime={this.state.currentTime} />
 				<TimerButton />
-				<TimerConfig 
-					baseTime = {this.state.baseTime}
-					setBaseTime={this.setBaseTime}
-				/>
+				{
+					(this.state.timerState !== timerStates.RUNNING)
+					&&
+					(<TimerConfig 
+						baseTime = {this.state.baseTime}
+						setBaseTime={this.setBaseTime}
+					/>)
+				}
 			</div>
 		);
 	}
